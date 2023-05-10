2b78ce00086b967db901d70f4b65f477bd9af835
LeoPradoG <108141253+LeoPradoG@users.noreply.github.com>
2023-05-10 08:48:06 -0300

    att
    att

--- veiculos.php
+++ veiculos.php
@@ -109,7 +109,7 @@ $res_cons_tabela_cor = oci_parse($conn_ora, $cons_tabela_cor);
 
     <!--MODAL-->
 
-    <div class="modal fade" id="cad_veiculo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade top_modal" id="cad_veiculo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
         <div class="modal-header">
